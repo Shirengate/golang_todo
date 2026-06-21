3036ddfd829b7486b55688b0fa04a8fc7493097e
Shirengate <sweetgames551ggg@gmail.com>
2026-06-21 13:56:01 +0300

    init ui-kit

---
+++ frontend/ui-kit/src/exports/icons.ts
@@ -0,0 +1,3 @@
+export * from "react-icons";
+export * from "react-icons/rx";
+export * from "react-icons/ri";
--- frontend/ui-kit/vite.config.ts
+++ frontend/ui-kit/vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
 import { libInjectCss } from "vite-plugin-lib-inject-css";
+import { resolve } from "path";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -15,14 +17,26 @@ export default defineConfig({
 
   build: {
     lib: {
-      entry: [
-        "src/exports/componets.ts",
-        "src/exports/icons.ts",
-        "src/exports/styles.ts",
-        "src/exports/vars.ts",
-      ],
+      entry: {
+        components: resolve(__dirname, "src/exports/components.ts"),
+        icons: resolve(__dirname, "src/exports/icons.ts"),
+        styles: resolve(__dirname, "src/exports/styles.ts"),
+        vars: resolve(__dirname, "src/exports/vars.ts"),
+      },
       formats: ["es", "cjs"],
-      fileName: (formt, entry) => ``,
+      fileName: (format, entryName) => {
+        if (format === "es") return `${entryName}.js`;
+        return `${entryName}.cjs`;
+      },
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
     },
   },
 });
